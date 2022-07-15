2f2c6799d57838d5f300234b7aa1208fd50d6a18
Adrian <acalerobarrero@gmail.com>
2022-07-15 20:06:31 +0200

    Change resp of store & localStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { iLogin, iProfesional } from "../interfaces/interfaces";
+import { iProfesional, iUser } from "../interfaces/interfaces";
 import { Review } from "../models/review.model";
 import { profesionalReducer } from "../redux/profesional-reducer/action.reducer";
 import { reviewReducer } from "../redux/review-reducer/action.reducer";
 import { userReducer } from "../redux/user-reducer/action.reducer";
 
 export interface iStore {
-    user: iLogin;
+    user: iUser;
     profesional: Array<iProfesional>;
     review: Array<Review>;
 }
 
 export const preloadedState: iStore = {
-    user: {} as iLogin,
+    user: {} as iUser,
     profesional: [] as Array<iProfesional>,
     review: [] as Array<Review>,
 };
