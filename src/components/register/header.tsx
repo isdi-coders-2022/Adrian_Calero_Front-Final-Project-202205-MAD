f3102485b4b8822069d60b949c0dd6f3dcb3ec20
Adrian <acalerobarrero@gmail.com>
2022-07-18 20:53:02 +0200

    Change className

--- src/components/register/header.tsx
+++ src/components/register/header.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export function HeaderRegister() {
     return (
-        <header className="authorization">
+        <header className="header-auth">
             <Typography variant="h6">FIX</Typography>
             <Typography variant="h2">YOUR</Typography>
             <Typography variant="h6">PROBLEM WITH</Typography>
