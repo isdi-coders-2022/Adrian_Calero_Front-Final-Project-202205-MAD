c0331901eaf9952e26714f637b8f1288cadaff6e
Adrian <acalerobarrero@gmail.com>
2022-07-19 20:36:33 +0200

    Add test of detail page

---
+++ src/pages/detail.spec.tsx
@@ -0,0 +1,70 @@
+import { BrowserRouter } from "react-router-dom";
+import { profesionalReducer } from "../redux/profesional-reducer/action.reducer";
+import { reviewReducer } from "../redux/review-reducer/action.reducer";
+import { userReducer } from "../redux/user-reducer/action.reducer";
+import { LocalStorage } from "../services/localStorage";
+import { render, screen } from "../services/test.utils";
+import Detail from "./detail";
+
+jest.mock("../services/localStorage");
+
+const reducer = {
+    profesional: profesionalReducer,
+    user: userReducer,
+    review: reviewReducer,
+};
+
+const mockProfesional = {
+    _id: "62cef4271854336fe7fe777f",
+    avatar: "https://firebasestorage.googleapis.com/v0/b/proyect-files.appspot.com/...",
+    name: "Jose",
+    profesion: "arquitect",
+    info: {
+        description:
+            "Construction companies, but they can also be self-employed individuals...",
+        price: 30,
+        img: "",
+        video: "https://firebasestorage.googleapis.com/v0/b/proyect-files.appspot.com/...",
+    },
+};
+
+const mockReview = {
+    _id: "62c41bec8bfe11181f9ebf54",
+    worker: "62c41172eb20556e8d48e754",
+    client: "62c40a97add2c6f044fc6874",
+    reviews: {
+        comment: "Guapo",
+        score: 5,
+    },
+};
+
+const preloadedState = {
+    profesional: [mockProfesional],
+    user: {
+        _id: "1",
+        avatar: "url",
+        userName: "test",
+        email: "test@test.com",
+        favorites: [],
+    },
+    review: [mockReview],
+};
+const login = { token: "1", id: "62c40a97add2c6f044fc6874" };
+
+describe("Given the page detail", () => {
+    beforeEach(() => {
+        LocalStorage.prototype.getItem = jest.fn().mockResolvedValue(login);
+    });
+    describe("When i render the page", () => {
+        test("Then it shoul be rendered", () => {
+            render(
+                <BrowserRouter>
+                    <Detail />
+                </BrowserRouter>,
+                { preloadedState, reducer }
+            );
+
+            expect(screen.getByText(/reviews/i)).toBeInTheDocument();
+        });
+    });
+});
